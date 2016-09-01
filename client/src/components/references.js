68b5e82638b8dffcd63ee591a7112599b4c9543d
Ryan <ryan@solomid.net>
2016-08-31 19:20:00 -0700

    minor style changes

--- client/src/components/references.js
+++ client/src/components/references.js
@@ -13,14 +13,15 @@ export const References = React.createClass({
                 <h3><FontAwesome name="check-square-o" inverse /> References</h3>
                 <br />
                 {this.props.references.map(function (ref, i) {
+                    const quote = <blockquote>
+                              <p className="m-b-0">"{ref.quote}"</p>
+                            </blockquote>;
                     return ( 
                         <div key={'reference-' + i}>
                             <h5>{ref.name}</h5> 
                             <h5><small className="text-muted">{ref.title} - {ref.company}</small></h5>
                             <h6><FontAwesome name="envelope" /> {ref.email}</h6>
-                            <blockquote>
-                              <p className="m-b-0">"{ref.quote}"</p>
-                            </blockquote>
+                            {quote}
                         </div> 
                     )
                 })}
