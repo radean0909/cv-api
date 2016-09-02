701f6102a54d534fa1bc8c882f2447a7f2d615c7
Ryan <ryan@solomid.net>
2016-09-02 08:55:34 -0700

    Removed {PDF} references

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,6 @@ const routes = (
 	<Route component={CVContainer} >
 		<Route path="/resume" component={CVContainer} />
 		<Route path="/" component={CVContainer} />
-		<Route path="/pdf" component={PDF} />
 	</Route>
 );
 
